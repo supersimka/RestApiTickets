6345321fe640278051aaac6e4e85467b4a7ee2a1
Marina Simonova <97460149+supersimka@users.noreply.github.com>
2023-02-06 09:15:50 +0300

    Update config.php

--- app/config.php
+++ app/config.php
@@ -1,6 +1,6 @@
 <?php
-    define('DB_HOST', 'localhost');
-    define('DB_USER', 'root');
-    define('DB_PASSWORD', 'root');
-    define('DB_NAME', 'restapi'); 
+    define('DB_HOST', '');
+    define('DB_USER', '');
+    define('DB_PASSWORD', '');
+    define('DB_NAME', ''); 
     define('DB_CHARSET', 'utf8');
